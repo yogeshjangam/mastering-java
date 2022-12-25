741a3080a1a78c679500f66fe64897a6f96183c4
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 19:25:47 +0530

    learnt && operators

--- src/Hello.java
+++ src/Hello.java
@@ -7,9 +7,15 @@ public class Hello {
             System.out.println("And i am not scared of aliens");
         }
         
-        int topScore = 100;
-        if(topScore >= 100) {
+        int topScore = 80;
+
+        if(topScore < 100) {
             System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 100;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
