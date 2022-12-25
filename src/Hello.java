a8693680eb0d676c37bed6a579fdfec153e6f6e6
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 19:59:59 +0530

    3-learning-logical-and-ternary-operators learn or (||) operator

--- src/Hello.java
+++ src/Hello.java
@@ -21,5 +21,11 @@ public class Hello {
         if ((topScore > 90) || (secondTopScore <= 90)){
             System.out.println("Either or both of the conditions are true");
         }
+
+        int newValue = 50;
+        // using equal operator gives error ==> if (newValue = 50) {
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
     }
 }
