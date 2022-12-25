8ad941ad6c9937c15d1e23115dafbe49a5aac34c
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 21:42:21 +0530

    4-expressions-statements using and outputting

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
     public static void main(String[] args) {
         double km = (100 * 1.609344);
         System.out.println("Kms is " + km);
+
+        int highScore = 50;
+
+        if (highScore > 25) {
+            highScore += 1000; // add bonus points
+        }
+
+        int health = 100;
+
+        if ((health < 25) && (highScore > 1000)) {
+            highScore -= 100;
+        }
     }
 }
