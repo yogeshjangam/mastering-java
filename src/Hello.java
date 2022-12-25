a6c86d78f7dde12364bf9b9b525a4c56f2e455f4
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 19:41:41 +0530

    3-learning-logical-and-ternary-operators fix one suggestion

--- src/Hello.java
+++ src/Hello.java
@@ -2,11 +2,11 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Yogesh");
         boolean isAlien = false;
-        if (isAlien == false) {
+        if (!isAlien) {
             System.out.println("It is not an alien!");
             System.out.println("And i am not scared of aliens");
         }
-        
+
         int topScore = 80;
 
         if(topScore < 100) {
