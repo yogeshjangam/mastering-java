51ed8fd1b9ec76e86ddc5a0ef743b633202d0be9
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 22:01:16 +0530

    4-expressions-statements using if,else-if,else blocks

--- src/LearnCodeBlocks.java
+++ src/LearnCodeBlocks.java
@@ -1,12 +1,18 @@
 public class LearnCodeBlocks {
     public static void main(String[] args) {
+
+
         boolean gameOver = true;
-        int score = 5000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score == 5000) {
-            System.out.println("Your score was 5000.");
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but greater than 1000.");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000.");
+        } else {
+            System.out.println("Got here");
         }
     }
 }
