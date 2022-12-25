e2a125954374df485af8bab5beb7a7093eb68e07
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 21:54:54 +0530

    4-expressions-statements using code blocks

---
+++ src/LearnCodeBlocks.java
@@ -0,0 +1,12 @@
+public class LearnCodeBlocks {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000) {
+            System.out.println("Your score was 5000.");
+        }
+    }
+}
