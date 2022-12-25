89be807ef063b1a39e1aab12e17fdc35ed228eb4
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 19:32:19 +0530

    update topscore to 81

--- src/Hello.java
+++ src/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 100;
+        int secondTopScore = 81;
         if (topScore > secondTopScore && topScore < 100){
             System.out.println("Greater than second top score and less than 100");
         }
