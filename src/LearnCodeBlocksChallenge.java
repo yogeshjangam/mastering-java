03ba71aa11ff084bf2918d9388543921a9164da5
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-25 22:10:28 +0530

    4-expressions-statements using existing variables and line comments

--- src/LearnCodeBlocksChallenge.java
+++ src/LearnCodeBlocksChallenge.java
@@ -13,17 +13,26 @@ public class LearnCodeBlocksChallenge {
             System.out.println("Your final score is " + finalScore);
         }
 
-        boolean newGameOver = true;
-        int newScore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
-        int newFinalScore = score;
+//        boolean newGameOver = true;
+//        int newScore = 10000;
+//        int newLevelCompleted = 8;
+//        int newBonus = 200;
+//        int newFinalScore = score;
+//
+//
+//        // Scenario 2
+//        if (newGameOver) {
+//            newFinalScore += newLevelCompleted * newBonus;
+//            System.out.println("Your final score *new* is " + newFinalScore);
+//        }
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        finalScore = score;
 
-
-        // Scenario 2
-        if (newGameOver) {
-            newFinalScore += newLevelCompleted * newBonus;
-            System.out.println("Your final score *new* is " + newFinalScore);
+        if (gameOver) {
+            finalScore += levelCompleted * bonus;
+            System.out.println("Your final score is " + finalScore);
         }
 
     }
