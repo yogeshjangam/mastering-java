781eb79ef49450071dd04f6d7d094384c59e648b
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-26 16:23:48 +0530

    4-expressions-statements adding some more methods

--- src/LearnMethodOverloading.java
+++ src/LearnMethodOverloading.java
@@ -2,6 +2,7 @@ public class LearnMethodOverloading {
     public static void main(String[] args) {
         int newScore = calculateScore("Time", 500);
         calculateScore(75);
+        calculateScore();
         System.out.println("New score is "+ newScore);
     }
 
@@ -14,4 +15,16 @@ public class LearnMethodOverloading {
         System.out.println("Unnamed player scored " + score + " points.");
         return score * 1000;
     }
+
+    public static int calculateScore()
+    {
+        System.out.println("Not player name, no player scored");
+        return 0;
+    }
+
+/*    public static void calculateScore()
+    {
+        System.out.println("Not player name, no player scored");
+
+    }*/
 }
