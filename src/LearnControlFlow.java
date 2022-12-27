774085c9459e9926c7c9ae2717a20fea6a3f1a46
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-27 08:15:57 +0530

    11-control-flow replacing with switch case

--- src/LearnControlFlow.java
+++ src/LearnControlFlow.java
@@ -2,12 +2,24 @@ public class LearnControlFlow {
     public static void main(String[] args) {
         int value = 2;
 
-        if (value == 1) {
+/*        if (value == 1) {
             System.out.println("value was 1");
         } else if (value == 2) {
             System.out.println("value was 2");
         } else {
             System.out.println("value was something other than 1 or 2");
+        }*/
+
+        switch (value) {
+            case 1:
+                System.out.println("value was 1");
+                break;
+            case 2:
+                System.out.println("value was 2");
+                break;
+            default:
+                System.out.println("value was something other than 1 or 2");
         }
+
     }
 }
