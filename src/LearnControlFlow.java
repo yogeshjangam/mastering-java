193bff9ed2968719d1f93203cbd62f26729571b2
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-27 08:11:42 +0530

    11-control-flow using if statements

---
+++ src/LearnControlFlow.java
@@ -0,0 +1,13 @@
+public class LearnControlFlow {
+    public static void main(String[] args) {
+        int value = 2;
+
+        if (value == 1) {
+            System.out.println("value was 1");
+        } else if (value == 2) {
+            System.out.println("value was 2");
+        } else {
+            System.out.println("value was something other than 1 or 2");
+        }
+    }
+}
