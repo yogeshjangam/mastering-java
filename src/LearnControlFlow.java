c70506f703aca0d4dd7ec908e47d4f99e51664b3
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-27 08:21:07 +0530

    11-control-flow replacing with enhanced switch

--- src/LearnControlFlow.java
+++ src/LearnControlFlow.java
@@ -11,16 +11,10 @@ public class LearnControlFlow {
         }*/
 
         switch (value) {
-            case 1:
-                System.out.println("value was 1");
-                break;
-            case 2:
-                System.out.println("value was 2");
-                break;
-            case 3: case 4: case 5:
-                System.out.println("was a 3 or 4 or 5,\nActually was a "+ value);
-            default:
-                System.out.println("value was something other than 1, 2, 3, 4 or 5");
+            case 1 -> System.out.println("value was 1");
+            case 2 -> System.out.println("value was 2");
+            case 3, 4, 5 -> System.out.println("was a 3 or 4 or 5,\nActually was a " + value);
+            default -> System.out.println("value was something other than 1, 2, 3, 4 or 5");
         }
 
     }
