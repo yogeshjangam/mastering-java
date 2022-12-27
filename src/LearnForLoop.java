81b0afe9a46cf1adb0072c66c3e580a683efe259
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-27 11:01:39 +0530

    11-control-flow added break

--- src/LearnForLoop.java
+++ src/LearnForLoop.java
@@ -3,7 +3,10 @@ public class LearnForLoop {
         for (int counter = 1; counter <= 5; counter++) {
             System.out.println(counter);
         }
-        for (double rate = 2.0; rate <= 5.0; rate += 0.25) {
+        for (double rate = 2.0; rate <= 9.0; rate += 0.25) {
+            if (rate == 7.5) {
+                break;
+            }
             System.out.println("100 at " + rate + "% interest rate is " + calculateInterest(100, rate));
         }
 
