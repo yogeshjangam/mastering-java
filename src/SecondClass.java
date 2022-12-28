de9f409f436afb9e93a040ef3c54179be675eb83
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-28 14:40:16 +0530

    11-control-flow using the do while loop

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,16 +1,22 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Second Class");
-        for (int i = 1; i <= 5 ; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
         System.out.println("Using while loop");
 
         int j = 1;
-        while (j <= 5)
-        {
+        while (j <= 5) {
             System.out.println(j);
             j++;
         }
+
+        System.out.println("Using do while loop");
+        int k = 1;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 5);
     }
 }
