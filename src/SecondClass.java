305a05c8633251ae17a04b83d09321c2b1d10c8a
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-28 19:43:13 +0530

    11-control-flow coding exercises

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,59 +1,61 @@
 public class SecondClass {
     public static void main(String[] args) {
 
-        System.out.println(isPalindrome(11));
-        System.out.println(getEvenDigitSum(123456789));
-        System.out.println("f+l for 252 ::" + sumFirstAndLastDigit(252));
-        System.out.println("f+l for 5 ::" + sumFirstAndLastDigit(5));
-        System.out.println("Second Class");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Using while loop");
+        hasSharedDigit(12, 23);
 
-        int j = 1;
-        while (j <= 5) {
-            System.out.println(j);
-            j++;
-        }
-
-        System.out.println("Using do while loop");
-        int k = 1;
-        do {
-            System.out.println(k);
-            k++;
-        } while (k <= 5);
-
-        int n = 0;
-        while (n < 50) {
-            n += 5;
-            if (n % 25 == 0) continue;
-            System.out.print(n + "_");
-        }
-
-        //Printing only even numbers
-        n = 0;
-        int countOfEven = 0;
-        int countOfOdd = 0;
-        while (n < 100) {
-            n++;
-            if (!isEven(n)) {
-                countOfOdd++;
-                continue;
-            }
-            System.out.println(n + " is an even number");
-            countOfEven++;
-            if (countOfEven >= 5) {
-                break;
-            }
-        }
-        System.out.println("Found " + countOfOdd + " odd numbers and " + countOfEven + " even numbers.");
-
-        System.out.println("Sum of digits 123 :" + sumDigits(123));
-        System.out.println("Sum of digits 1 :" + sumDigits(1));
-        System.out.println("Sum of digits 12 :" + sumDigits(12));
-        System.out.println("Sum of digits 145643 :" + sumDigits(14563));
-        System.out.println("Sum of digits 100 :" + sumDigits(100));
+//        System.out.println(isPalindrome(11));
+//        System.out.println(getEvenDigitSum(123456789));
+//        System.out.println("f+l for 252 ::" + sumFirstAndLastDigit(252));
+//        System.out.println("f+l for 5 ::" + sumFirstAndLastDigit(5));
+//        System.out.println("Second Class");
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+//        System.out.println("Using while loop");
+//
+//        int j = 1;
+//        while (j <= 5) {
+//            System.out.println(j);
+//            j++;
+//        }
+//
+//        System.out.println("Using do while loop");
+//        int k = 1;
+//        do {
+//            System.out.println(k);
+//            k++;
+//        } while (k <= 5);
+//
+//        int n = 0;
+//        while (n < 50) {
+//            n += 5;
+//            if (n % 25 == 0) continue;
+//            System.out.print(n + "_");
+//        }
+//
+//        //Printing only even numbers
+//        n = 0;
+//        int countOfEven = 0;
+//        int countOfOdd = 0;
+//        while (n < 100) {
+//            n++;
+//            if (!isEven(n)) {
+//                countOfOdd++;
+//                continue;
+//            }
+//            System.out.println(n + " is an even number");
+//            countOfEven++;
+//            if (countOfEven >= 5) {
+//                break;
+//            }
+//        }
+//        System.out.println("Found " + countOfOdd + " odd numbers and " + countOfEven + " even numbers.");
+//
+//        System.out.println("Sum of digits 123 :" + sumDigits(123));
+//        System.out.println("Sum of digits 1 :" + sumDigits(1));
+//        System.out.println("Sum of digits 12 :" + sumDigits(12));
+//        System.out.println("Sum of digits 145643 :" + sumDigits(14563));
+//        System.out.println("Sum of digits 100 :" + sumDigits(100));
     }
 
     public static boolean isEven(int n) {
@@ -134,5 +136,38 @@ public class SecondClass {
 
         } while (n != 0);
         return result;
+    }
+
+    public static boolean hasSharedDigit(int a, int b) {
+        boolean result = false;
+
+        int a1 = a % 10;
+        int a2 = (a / 10) % 10;
+        int b1 = b % 10;
+        int b2 = (b / 10) % 10;
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(b1);
+        System.out.println(b2);
+
+        return (a1 == b1 || a1 == b2 || a2 == b1 || a2 == b2);
+    }
+
+    public static boolean hasSameLastDigit(int a, int b, int c) {
+        return ((a % 10) == (b % 10) && (b % 10) == (c % 10));
+    }
+
+    public static int printFactors(int n)
+    {
+        int result = 0;
+        if (n < 1) {
+            System.out.println("Invalid Value");
+        }
+        else
+        {
+
+        }
+        return  result;
     }
 }
