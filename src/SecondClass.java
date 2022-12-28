8c10880523554cfb6e4ad6b43f1f91411b530e07
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-28 17:08:06 +0530

    11-control-flow coding exercises

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,5 +1,10 @@
 public class SecondClass {
     public static void main(String[] args) {
+
+        System.out.println(isPalindrome(11));
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println("f+l for 252 ::" + sumFirstAndLastDigit(252));
+        System.out.println("f+l for 5 ::" + sumFirstAndLastDigit(5));
         System.out.println("Second Class");
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
@@ -68,6 +73,65 @@ public class SecondClass {
             result += d;
             System.out.println("digit :" + d);
             n = n / 10;
+        } while (n != 0);
+        return result;
+    }
+
+    public static boolean isPalindrome(int n) {
+        int result = 0;
+        int originalNumber = n;
+        int d = 0;
+        do {
+            d = n % 10;
+            result = result * 10 + d;
+            n = n / 10;
+        } while (n != 0);
+
+
+        return (result == originalNumber);
+    }
+
+    public static int sumFirstAndLastDigit(int n) {
+        int result = 0;
+        if (n < 0) {
+            return -1;
+        }
+        int firstDigit = 0;
+        int lastDigit = 0;
+
+        int d = 0;
+        int cd = 0;
+        do {
+            d = n % 10;
+            cd++;
+
+
+            result = result * 10 + d;
+            n = n / 10;
+            if (cd == 1) firstDigit = d;
+            lastDigit = d;
+        } while (n != 0);
+
+        return (firstDigit + lastDigit);
+
+    }
+
+    public static int getEvenDigitSum(int n) {
+        int result = 0;
+        if (n < 0) {
+            return -1;
+        }
+
+        int d = 0;
+
+        do {
+            d = n % 10;
+            if (n % 2 == 0) {
+                result = result + d;
+            }
+
+            n = n / 10;
+
         } while (n != 0);
         return result;
     }
