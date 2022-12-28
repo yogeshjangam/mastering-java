596a396b27974d6331f206a329ca582e422b41a3
Yogesh Jangam <yogesh.jangam@gmail.com>
2022-12-28 15:03:35 +0530

    11-control-flow even odd numbers solve

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -20,11 +20,32 @@ public class SecondClass {
         } while (k <= 5);
 
         int n = 0;
-        while ( n <50)
-        {
-            n+=5;
-            if(n % 25 == 0 ) continue;
-            System.out.print(n+"_");
+        while (n < 50) {
+            n += 5;
+            if (n % 25 == 0) continue;
+            System.out.print(n + "_");
         }
+
+        //Printing only even numbers
+        n = 0;
+        int countOfEven = 0;
+        int countOfOdd = 0;
+        while (n < 100) {
+            n++;
+            if (!isEven(n)) {
+                countOfOdd++;
+                continue;
+            }
+            System.out.println(n + " is an even number");
+            countOfEven++;
+            if (countOfEven >= 5) {
+                break;
+            }
+        }
+        System.out.println("Found " + countOfOdd + " odd numbers and " + countOfEven + " even numbers.");
+    }
+
+    public static boolean isEven(int n) {
+        return (n % 2 == 0);
     }
 }
